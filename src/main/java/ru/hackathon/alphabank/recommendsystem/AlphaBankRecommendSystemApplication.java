7ffbd9fed89e013f35e8ab0ffdb1b04008ee6488
Evgeniy PC <79169380904@mail.ru>
2024-11-29 23:02:37 +0300

    Init commit

---
+++ src/main/java/ru/hackathon/alphabank/recommendsystem/AlphaBankRecommendSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.hackathon.alphabank.recommendsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaBankRecommendSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaBankRecommendSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hackathon/alphabank/recommendsystem/AlphaBankRecommendSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hackathon.alphabank.recommendsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaBankRecommendSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
